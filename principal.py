e672062efa650d873660270e19f7dd631a082c16
BrayanRestrepo <brestrepoisaza@gmail.com>
2022-09-17 12:53:28 -0500

    se corrigen conflictos

--- principal.py
+++ principal.py
@@ -1,8 +1,5 @@
 print("hola soy JUAN GALLEGO EL COMMITTER")
-<<<<<<< HEAD
 print("Hola soy Brayan el back-end jr")
-=======
 print("Hola  soy Kevin y soy un desarollador JR")
->>>>>>> f71f5826b3c9d09d85677335adb4ad1ff7077c44
 
 print("Hola, soy Jenn y soy developer junior")
