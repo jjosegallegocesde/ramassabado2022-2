6806105d07570a223ba0b984ac554da48de0a42d
Macbast <danielbastidas459@gmail.com>
2022-09-17 12:47:44 -0500

    pepe

--- principal.py
+++ principal.py
@@ -1,7 +1,4 @@
 print("hola soy JUAN GALLEGO EL COMMITTER")
-<<<<<<< HEAD
 print("hola soy Daniel Alexander ARENAS Gaviria El DBA")
-=======
 
 print("Hola, soy Jenn y soy developer junior")
->>>>>>> 9a2560898d22dd949de49f840b1c6c8cea2267c3
